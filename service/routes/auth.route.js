9c405e4223d8cf6e1a38b5b010c43ccc532204a7
NguyenTama <codewithyangyang@gmail.com>
2025-03-10 08:04:52 +0100

    Modify UI header in FE, add function logout, get profile

--- service/routes/auth.route.js
+++ service/routes/auth.route.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
-const {authLogin, refreshAccessToken, firstTimeChangePassword} = require('../controllers/auth.controller');
+const {authLogin, refreshAccessToken, firstTimeChangePassword, authLogout, getProfile} = require('../controllers/auth.controller');
+const {authenticate} = require("../middlewares/auth.middleware");
 
 router.post('/login', authLogin);
 
 router.get('/get-access-token', refreshAccessToken);
 
 router.post('/first-time-change-password', firstTimeChangePassword);
+
+router.get("/logout", authLogout);
+
+router.get("/get-profile", authenticate, getProfile);
 
 module.exports = router;
