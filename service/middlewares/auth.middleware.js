552a7a8d7d33e730b43eb3ab8724f66d53dbdf0d
NguyenTama <codewithyangyang@gmail.com>
2025-03-10 16:39:23 +0100

    fix error refresh token

--- service/middlewares/auth.middleware.js
+++ service/middlewares/auth.middleware.js
@@ -1,58 +1,63 @@
 const jwt = require('jsonwebtoken');
 const User = require("../models/user");
 
-
 const authenticate = async (req, res, next) => {
-  try {
-    const { accessToken } = req.cookies;
-    if (!accessToken) {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized"
-      });
+    try {
+        const {accessToken} = req.cookies;
+        if (!accessToken) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized"
+            });
+        }
+
+        jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, async (err, decoded) => {
+            if (err) {
+                if (err.name === "TokenExpiredError") {
+                    return res.status(401).json({
+                        success: false,
+                        message: "token_expired",
+                    });
+                }
+                return res.status(401).json({
+                    success: false,
+                    message: "Unauthorized"
+                });
+            }
+
+            const user = await User.findById(decoded.id).select("-password -__v ");
+            if (!user) {
+                return res.status(404).json({
+                    success: false,
+                    message: "User not found!"
+                });
+            }
+            req.user = user;
+            next();
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            success: false,
+            message: e.message || "Internal Server Error"
+        });
     }
-
-    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, async (err, decoded) => {
-      if (err || !decoded?.id) {
-        return res.status(401).json({
-          success: false,
-          message: "Unauthorized"
-        });
-      }
-
-      const user = await User.findById(decoded.id).select("-password -__v ");
-      if (!user) {
-        return res.status(404).json({
-          success: false,
-          message: "User not found!"
-        });
-      }
-      req.user = user;
-      next();
-    });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      success: false,
-      message: e.message || "Internal Server Error"
-    });
-  }
 };
 const isAdmin = async (req, res, next) => {
-  try {
-    if (req?.user?.role === "ADMIN") {
-      next();
-    } else {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized",
-      });
+    try {
+        if (req?.user?.role === "ADMIN") {
+            next();
+        } else {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized",
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: e.message || "Internal Server Error",
+        });
     }
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: e.message || "Internal Server Error",
-    });
-  }
 };
-module.exports = { authenticate, isAdmin };
+module.exports = {authenticate, isAdmin};
